641c519889d9bdc7b22777f7316abf509e030b11
Lihat Ervin <erwyn86@yahoo.com>
2018-02-28 14:40:47 +0200

    Init commit

---
+++ server/server.js
@@ -0,0 +1,28 @@
+
+const path = require('path');
+const express = require('express');
+
+const publicPath = path.join(__dirname, '/../public');
+
+const port = process.env.PORT || 3000;
+
+//require(path.join(__dirname,'../config/config.json'));
+
+
+
+var app = express();
+
+app.use(express.static(publicPath));
+
+
+
+app.get('/', (req, res) => {
+
+});
+
+
+app.listen(port, () => {
+  console.log(`Sarted on port ${port}`);
+});
+
+module.exports = { app }
